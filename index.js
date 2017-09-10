7475a4acfa3aaf0b20d645ce97789eae50908412
Anthony Fields <janthonyfields@gmail.com>
2017-09-10 02:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber( katzDeliLine, name ) {
   katzDeliLine.push( name );
 
-  return `Welcome, ${name}. You are number ${ katzDeliLine.indexOf(name) } in line.`;
+  return `Welcome, ${name}. You are number ${ katzDeliLine.indexOf(name) + 1 } in line.`;
 }
 
 // `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)} in line.`
