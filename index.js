ae96ff14afc55883abdb29618a399719f58dd46d
Anthony Fields <janthonyfields@gmail.com>
2017-09-10 02:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ function takeANumber( katzDeliLine, name ) {
   return `Welcome, ${name}. You are number ${ katzDeliLine.indexOf(name) + 1 } in line.`;
 }
 
-// `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)} in line.`
+function nowServing() {
+  returnFirstPerson();
+  katzDeliLine.shift();
+  
+  function returnFirstPerson() {
+    return `Currently serving ${ katzDeliLine[0] }.`;
+  }
+}
+// "There is nobody waiting to be served!"
